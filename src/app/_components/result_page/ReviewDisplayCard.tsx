7bbde9d53a418b38551bc548d3486d0aca0f039d
Gabriel <gabriel.anguiano13x@gmail.com>
2024-02-04 19:30:19 -0600

    fixed date on review breakin out of card

--- src/app/_components/result_page/ReviewDisplayCard.tsx
+++ src/app/_components/result_page/ReviewDisplayCard.tsx
@@ -36,7 +36,7 @@ const ReviewDisplayCard = ({
 }: PropsI) => {
   return (
     <div className="bg-gray-100 w-3/5 p-8 mt-6">
-      <div className="relative w-1/2 left-200">
+      <div className="relative w-full flex flex-row-reverse">
         <div className="font-bold text-sm">{`${
           Month[date.getMonth()]
         } ${date.getDay()}, ${date.getFullYear()}`}</div>
